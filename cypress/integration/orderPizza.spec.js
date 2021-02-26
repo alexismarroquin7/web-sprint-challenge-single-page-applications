e39f71f54dcdae2dae2c483580953c217a715c15
Alexis Marroquin <alexismarroquin754@gmail.com>
2021-02-26 10:49:09 -0800

    Test | can select multiple toppings

--- cypress/integration/orderPizza.spec.js
+++ cypress/integration/orderPizza.spec.js
@@ -5,10 +5,34 @@ describe('Pizza', () => {
 
     const pizzaButton = () => cy.get('#goToOrderForm')
     const nameInput = () => cy.get('input[name=name]');
+    const pepperoniInput = () => cy.get('input[name=pepperoni]')
+    const mushroomInput = () => cy.get('input[name=mushroom]')
+    const hamInput = () => cy.get('input[name=ham]')
+    const olivesInput = () => cy.get('input[name=olives]')
 
-    it('Type text in name input', () => {
-        pizzaButton().click();
-        nameInput().type('Alexis Marroquin');
-        nameInput().should('have.value', 'Alexis Marroquin');
+    describe('Can type in inputs', () => {
+        it('Type text in name input', () => {
+            pizzaButton().click();
+            nameInput().type('Alexis Marroquin');
+            nameInput().should('have.value', 'Alexis Marroquin');
+        })
     })
+
+    describe('Checking checkbox inputs', () => {
+        it('Select multiple toppings', () => {
+            pizzaButton().click();
+            
+            pepperoniInput().check();
+            mushroomInput().check();
+            hamInput().check();
+            olivesInput().check();
+
+            pepperoniInput().should('be.checked');
+            mushroomInput().should('be.checked');
+            hamInput().should('be.checked');
+            olivesInput().should('be.checked');
+        })
+    })
+
+
 })
